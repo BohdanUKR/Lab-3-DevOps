32d099ac95c3a356e20dfe1eeb4fc46214de21ab
Bohdan Drozd <drozd.bogdan35@gmail.com>
2023-07-11 21:24:12 -0400

    Added a Jenkinsfile

--- src/main/java/com/example/Lab2Q3_DevOps/Lab2Q3DevOpsApplication.java
+++ src/main/java/com/example/Lab2Q3_DevOps/Lab2Q3DevOpsApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab2Q3DevOpsApplication
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Lab2Q3DevOpsApplication.class, args);
+		System.out.println("Welcome to COMP367");
 	}
 
 }
